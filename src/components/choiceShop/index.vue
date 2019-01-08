dd9c70d55b418ebbdcf3f1404590c9595bc0a988
guanxf718 <xiufeng_guan@163.com>
2019-01-08 14:39:53 +0800

    add

---
+++ src/components/choiceShop/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>
+            <section>
+                
+            </section>
+            <section>
+                <h2>八号当铺</h2>
+                <p>距离您5.9km</p>
+            </section>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
