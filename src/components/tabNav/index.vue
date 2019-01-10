c335018d32bf367191c514622c30a04648b8bcce
guanxf718 <xiufeng_guan@163.com>
2019-01-10 16:48:03 +0800

    add

---
+++ src/components/tabNav/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <van-tabs v-model="active">
+        <van-tab v-for="el in innerList" :key="el.index" :title="el.text">
+            <slot></slot>
+        </van-tab>
+    </van-tabs>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            active: 0
+        }
+    },
+    props: {
+        innerList: {
+            type: Array
+        }
+    },
+    watch: {
+        active(e){
+            this.$emit('aaa',e);
+            console.log(this);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
